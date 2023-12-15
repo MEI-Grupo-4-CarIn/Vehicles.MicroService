624265bced368d02856efefbba45d1fdb1199e68
Duarte Fernades <duartejose99@hotmail.com>
2023-12-15 00:27:40 +0000

    wip+++

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,21 +1,56 @@
 const jwt = require('jsonwebtoken');
 
+const PUB_KEY = process.env.JWT_PUBLIC_KEY.replace(/\\n/g, '\n');
+const SECRET_KEY = process.env.SERVICE_SECRET_KEY;
+
 function authMiddleware(allowedRoles) {
     return function (req, res, next) {
-        const authHeader = req.headers.authorization;
-        if (authHeader) {
-            const token = authHeader.split(' ')[1];
-            const PUB_KEY = process.env.JWT_PUBLIC_KEY.replace(/\\n/g, '\n');
+        const userAuthHeader = req.headers.authorization;
+        const serviceAuthHeader = req.headers['service-authorization'];
+        
+        if (userAuthHeader) {
+            const token = userAuthHeader.split(' ')[1];
             jwt.verify(token, PUB_KEY, { algorithms: ['RS256'] }, (err, user) => {
-                if (err) {
-                    return res.status(403).send({ message: 'Invalid token.' });
-                }
-                // Check if the user's role is allowed
-                if (allowedRoles.includes(user['http://schemas.microsoft.com/ws/2008/06/identity/claims/role'])) {
-                    req.user = user;
-                    next();
+                if (!err) {
+                    // Check if the user's role is allowed
+                    if (allowedRoles.includes(user.role)) {
+                        req.user = user;
+                        next();
+                    } else {
+                        res.status(403).send({ message: 'User role not allowed.' });
+                    }
+                } else if (serviceAuthHeader) {
+                    const token = serviceAuthHeader.split(' ')[1];
+                    jwt.verify(token, SECRET_KEY, { algorithms: ['HS256'] }, (err, service) => {
+                        if (!err) {
+                            // Check if the service is allowed
+                            if (service.service === 'Routes.MicroService') {
+                                req.service = service;
+                                next();
+                            } else {
+                                res.status(403).send({ message: 'Invalid service.' });
+                            }
+                        } else {
+                            res.status(403).send({ message: 'Invalid service token.' });
+                        }
+                    });
                 } else {
-                    res.status(403).send({ message: 'You do not have the required role to access this resource.' });
+                    res.status(403).send({ message: 'Invalid user token.' });
+                }
+            });
+        } else if (serviceAuthHeader) {
+            const token = serviceAuthHeader.split(' ')[1];
+            jwt.verify(token, SECRET_KEY, { algorithms: ['HS256'] }, (err, service) => {
+                if (!err) {
+                    // Check if the service is allowed
+                    if (service.service === 'Routes.MicroService') {
+                        req.service = service;
+                        next();
+                    } else {
+                        res.status(403).send({ message: 'Invalid service.' });
+                    }
+                } else {
+                    res.status(403).send({ message: 'Invalid service token.' });
                 }
             });
         } else {
