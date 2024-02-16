65cacb01fa12dccd237b57f67799be4498dcbd94
Rafael Azevedo <a21197@alunos.ipca.pt>
2024-02-16 14:58:31 +0000

    Delete_Message_And_No_Vehicles_Message

--- src/interface_adapters/controllers/vehicleController.js
+++ src/interface_adapters/controllers/vehicleController.js
@@ -13,7 +13,7 @@ class VehicleController {
             Logger.info(`Vehicle created by the user '${req.user.email}' with success! Info: '${vehicle.id}'.`);
             res.status(201).json(vehicle);
         } catch (error) {
-            Logger.error(`Error creating vehicle: ${error.message}`);
+            Logger.error(`Error creating vehicle: ${error.message}`,error);
             res.status(400).json({ message: error.message });
         }
     }
@@ -25,7 +25,7 @@ class VehicleController {
             Logger.info(`Vehicle '${id}' successfully updated by the user '${req.user.email}'.`)
             res.status(200).json(updatedVehicle);
         } catch (error) {
-            Logger.error(`Error updating vehicle '${req.params.id}': ${error.message}`);
+            Logger.error(`Error updating vehicle '${req.params.id}': ${error.message}`,error);
             res.status(400).json({ message: error.message });
         }
     }
@@ -35,9 +35,9 @@ class VehicleController {
             const { id } = req.params;
             await this.vehiclePersistence.delete(id);
             Logger.info(`Vehicle '${id}' deleted by the user '${req.user.email}'.`)
-            res.status(204).send();
+            res.status(200).json({ message: "Vehicle has been successfully deleted." });
         } catch (error) {
-            Logger.error(`Error deleting vehicle '${req.params.id}': ${error.message}`);
+            Logger.error(`Error deleting vehicle '${req.params.id}': ${error.message}`,error);
             res.status(400).json({ message: error.message });
         }
     }
@@ -48,7 +48,7 @@ class VehicleController {
             const vehicle = await this.vehiclePersistence.getById(id);
             res.status(200).json(vehicle);
         } catch (error) {
-            Logger.error(`Error obtaining vehicle '${req.params.id}': ${error.message}`);
+            Logger.error(`Error obtaining vehicle '${req.params.id}': ${error.message}`,error);
             if (error instanceof NotFoundError) {
                 return res.status(404).json({ message: error.message });
             } else {
@@ -63,7 +63,7 @@ class VehicleController {
             const vehicle = await this.vehiclePersistence.getByLicensePlate(licensePlate);
             res.status(200).json(vehicle);
         } catch (error) {
-            Logger.error(`Error obtaining vehicle '${req.params.licensePlate}': ${error.message}`);
+            Logger.error(`Error obtaining vehicle '${req.params.licensePlate}': ${error.message}`,error);
             if (error instanceof NotFoundError) {
                 return res.status(404).json({ message: error.message });
             } else {
@@ -75,12 +75,16 @@ class VehicleController {
     async getAllVehicles(req, res) {
         try {
             const vehicles = await this.vehiclePersistence.getAllVehicles();
+            if (vehicles.length === 0) {
+                return res.status(404).json({ message: 'No vehicles found.' });
+            }
             res.status(200).json(vehicles);
         } catch (error) {
-            Logger.error(`Error obtaining vehicles list '${req.params.id}': ${error.message}`);
+            Logger.error(`Error obtaining vehicles list '${req.params.id}': ${error.message}`,error);
             res.status(500).json({ message: error.message });
         }
     }
+    
 }
 
 module.exports = VehicleController;
